ff79886969a78f9f3cd31c5bdd30049f83eb81a5
Jeremy Kim <jedo@alumni.unc.edu>
2025-03-31 15:05:33 -0400

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Jeremy Kim on 3/31/25.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OnboardingFlowTests/OnboardingFlowTests.swift
@@ -0,0 +1,16 @@
+//
+//  OnboardingFlowTests.swift
+//  OnboardingFlowTests
+//
+//  Created by Jeremy Kim on 3/31/25.
+//
+
+import Testing
+
+struct OnboardingFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
